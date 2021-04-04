d5c05f52d03130d7e113cdd584e37a5d1aa6c09c
Ashley Ramsey <ash@fastmail.us>
2021-04-04 11:40:28 -0700

    Set up client-side socketio

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import Chat from "../src/components/index";
 
-function Home(props) {
-    return (
-        <div>Welcome!</div>
-    )
+import io from 'socket.io-client';
+const socket = io("http://localhost:3000");
+
+function Home() {
+    const [chatLog, setChatLog] = useState([]);
+
+    useEffect(() => {
+        socket.on("chat message", (message) => {
+            setChatLog([...chatLog, message])
+        })
+
+        return () => {
+            socket.off('chat message');
+          };
+    })
+
+    const onSend = (message) => {
+        socket.emit("chat message", message);
+        console.log("sent message");
+    };
+
+    return <Chat chatLog={chatLog} onSend={onSend}/>;
 }
 
 export default Home;
