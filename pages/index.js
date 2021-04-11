876d9a7a79f64dcb0e77057c7793a66bec8b9392
Ashley Ramsey <ash@fastmail.us>
2021-04-10 21:06:58 -0700

    Refactor into single page
    Having a separate /chat page makes things difficult to manage if they go straight to that page

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,77 @@
-import React from "react";
-import { useRouter } from "next/router";
+import React, { useEffect, useState } from "react";
 import SignIn from "../src/components/SignIn";
 import PropTypes from "prop-types";
+import { useQuery } from "react-query";
+import apiCall from "../src/apiCall";
+import ChatRoom from "../src/components/ChatRoom";
+import { Grow } from "@material-ui/core";
 
-function SignInPage(props) {
+function Home(props) {
     const { socket } = props;
-    const router = useRouter();
+
+    // For sign in view
+    const [user, setUser] = useState(null);
+
+    // For chat view
+    const [chatLog, setChatLog] = useState([]);
+    const [users, setUsers] = useState([]);
 
     const onSignIn = (name, color) => {
-        socket.emit("add user", {name, color});
-        router.push("/chat");
+        socket.emit("add user", { name, color });
+        setUser(name);
     };
 
-    return <SignIn onSignIn={onSignIn}/>;
+    useQuery({
+        queryKey: "users",
+        queryFn: () => apiCall("/api/users"),
+        enabled: !!user,
+        onSuccess: (resp) => setUsers(resp),
+    });
+
+    useEffect(() => {
+        socket.on("chat message", (data) => {
+            setChatLog([...chatLog, data]);
+        });
+
+        socket.on("user joined", (data) => {
+            const message = `${data.username} joined the chat (${data.numUsers} total)`;
+            setChatLog([...chatLog, { ...data, message }]);
+            setUsers([...users, { username: data.username }]);
+        });
+
+        socket.on("user left", (data) => {
+            const message = `${data.username} left the chat (${data.numUsers} total)`;
+            setChatLog([...chatLog, { ...data, message }]);
+
+            const remainingUsers = users.filter(
+                (user) => user.username !== data.username
+            );
+            setUsers(remainingUsers);
+        });
+
+        return () => {
+            socket.off("chat message");
+        };
+    });
+
+    const onSend = (message) => {
+        socket.emit("chat message", message);
+        console.log("sent message");
+    };
+
+    if (!user) {
+        return <SignIn onSignIn={onSignIn} />;
+    }
+
+    return (
+        <Grow in={!!user}>
+            <ChatRoom chatLog={chatLog} users={users} onSend={onSend} />
+        </Grow>
+    );
 }
 
-export default SignInPage;
+export default Home;
 
-SignInPage.propTypes = {
+Home.propTypes = {
     socket: PropTypes.object.isRequired, // socket.io client
-}
+};
