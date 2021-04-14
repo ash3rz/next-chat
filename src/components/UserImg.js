b35f3e13d6256f38d39245abd78a7b570e263a9c
Ashley Ramsey <ash@fastmail.us>
2021-04-13 19:32:31 -0700

    Refactor to use RoboHash directly

--- src/components/UserImg.js
+++ src/components/UserImg.js
@@ -1,14 +1,24 @@
-import { MD5 } from "crypto-js";
+import { makeStyles } from "@material-ui/core";
 import PropTypes from "prop-types";
 import React from "react";
 
+const useStyles = makeStyles(() => ({
+    flipped: {
+        transform: "scaleX(-1)",
+        "-webkit-transform": "scaleX(-1)",
+    },
+}));
+
 function UserImg(props) {
     const { name } = props;
+    const classes = useStyles();
 
-    const hash = MD5(name);
-    const gravatarUrl = `https://www.gravatar.com/avatar/${hash}?s=45&d=robohash`;
+    const setNum = "set1";
+    const size = "50";
 
-    return <img src={gravatarUrl} alt="avatar" />;
+    const imgUrl = `https://www.robohash.org/${name}?set=${setNum}&size=${size}x${size}`;
+
+    return <img className={classes.flipped} src={imgUrl} alt="avatar" />;
 }
 
 export default UserImg;
